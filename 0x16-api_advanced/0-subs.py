37672aefbe18895ef04ef5f5496c57ac8d96cb4f
Pacific1990 <abet.eko@gmail.com>
2024-08-14 04:18:43 +0000

    Commit 001

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:\v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    headers = {'User-Agent': '0x16-api_advanced:project:\v1.0.0 (by /u/Fluffy_Archer_7840)'}
 
     response = requests.get(url, headers=headers)
 
