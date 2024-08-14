21e2ff09939fbe0906b62ef8b36b1584c1babde7
Pacific1990 <abet.eko@gmail.com>
2024-08-14 03:59:04 +0000

    Commit 001

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,15 @@ import requests
 def number_of_subscribers(subreddit):
 
     """returns the number of subscribers for a given subreddit"""
-    
+
     if subreddit is None or type(subreddit) is not str:
         return 0
-    
+
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced:project:\v1.0.0 (by /u/firdaus_cartoon_jr)'}
 
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
