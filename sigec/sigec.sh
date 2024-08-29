1be293737f0c71ea023f6a7ec16aa74cc09fb095
CPNTIC-Dev <108671773+CPNTIC-Dev@users.noreply.github.com>
2024-08-29 13:54:40 +0000

    commit 001

--- sigec/sigec.sh
+++ sigec/sigec.sh
@@ -24,24 +24,30 @@ base_name=$(echo $response | jq -r '.data.start')
 base_name=$(replace_spaces "$base_name")
 
 # Créer le dossier principal
-mkdir -p "$base_name"
+mkdir -p "./$base_name"
 # echo "Dossier de base : $base_name"
 
 # Lire les niveaux
 echo "$response" | jq -c '.data.levels[]' | while read -r level; do
     level_name=$(echo "$level" | jq -r '.name')
     level_name=$(replace_spaces "$level_name")
-    
     # Créer le sous-dossier pour chaque niveau
     level_path="$base_name/$level_name"
-    mkdir -p "$base_name/$level_path"
+    mkdir -p "./$base_name/$level_path"
 
     # Lire les examens dans le niveau
     echo "$level" | jq -c '.exams[]' | while read -r exam; do
         exam_name=$(echo "$exam" | jq -r '.specialty_name')
         exam_name=$(replace_spaces "$exam_name")
-        
         # Créer le sous-dossier pour chaque examen
-        mkdir -p "$base_name/$level_path/$exam_name"
+        mkdir -p "./$base_name/$level_path/$exam_name"
+
+        # Lire les salles dans l'examen
+        echo "$exam" | jq -c '.rooms[]' | while read -r room; do
+            room_name=$(echo "$room" | jq -r '.name')
+            room_name=$(replace_spaces "$room_name")
+            # Créer le sous-dossier pour chaque salle
+            mkdir -p "./$base_name/$level_path/$exam_name/$room_name"
+        done
     done
 done
