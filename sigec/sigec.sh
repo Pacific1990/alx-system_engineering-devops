b30cf35ec8397f74dead3218098f06b645166d1f
CPNTIC-Dev <108671773+CPNTIC-Dev@users.noreply.github.com>
2024-08-29 11:24:15 +0000

    Commit 002

--- sigec/sigec.sh
+++ sigec/sigec.sh
@@ -16,7 +16,7 @@ data='{"key1":"valeur1", "key2":"valeur2"}'
 response=$(curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" -d "$data" "$url")
 
 # Afficher la réponse
-echo "Réponse de l'API : $response"
+# echo "Réponse de l'API : $response"
 
-year = $(echo $response | jq -r '.data.start')
-echo "Ann�e scolaire : $year"
+year=$(echo $response | jq -r '.data.start')
+echo "Annee scolaire : $year"
