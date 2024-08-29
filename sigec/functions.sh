c362f9fff029e19aaf0e64fc2379ee1541f9506e
Eko Abet <abet.eko@gmail.com>
2024-08-29 22:23:21 +0000

    Update functions.sh

--- sigec/functions.sh
+++ sigec/functions.sh
@@ -1,4 +1,10 @@
 # Fonction pour remplacer les espaces par des soulignements
 replace_spaces() {
-    echo "$1" | tr ' ' '_'
+    # echo "$1" | tr ' ' '_'
+    local input="$1"
+    # Supprimer les espaces en début et en fin
+    input=$(echo "$input" | sed 's/^[ \t]*//;s/[ \t]*$//')
+    # Remplacer les espaces restants par des blancs soulignés
+    input=$(echo "$input" | tr ' ' '_')
+    echo "$input"
 }
