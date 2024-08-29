789dd08749083eddb3d85e42328d7957c3976fb4
CPNTIC-Dev <108671773+CPNTIC-Dev@users.noreply.github.com>
2024-08-29 12:41:46 +0000

    commit 002

--- sigec/sigec.sh
+++ sigec/sigec.sh
@@ -19,26 +19,22 @@ response=$(curl -s -X GET -H "Authorization: Bearer $token" -H "Content-Type: ap
 # Afficher la réponse
 # echo "Réponse de l'API : $response"
 
-# year=$(echo $response | jq -r '.data.start')
-# echo "Annee scolaire : $year"
-
 # Lire le nom principal
-# base_name=$(jq -r '.data.name' "$response")
 base_name=$(echo $response | jq -r '.data.start')
 base_name=$(replace_spaces "$base_name")
 
 # Créer le dossier principal
 mkdir -p "$base_name"
-echo "Dossier de base : $base_name"
+# echo "Dossier de base : $base_name"
 
 # Lire les niveaux
-jq -c '.data.levels[]' "$response" | while read -r level; do
+echo "$response" | jq -c '.data.levels[]' | while read -r level; do
     level_name=$(echo "$level" | jq -r '.name')
     level_name=$(replace_spaces "$level_name")
     
     # Créer le sous-dossier pour chaque niveau
     level_path="$base_name/$level_name"
-    mkdir -p "$level_path"
+    mkdir -p "$base_name/$level_path"
 
     # Lire les examens dans le niveau
     echo "$level" | jq -c '.exams[]' | while read -r exam; do
@@ -46,18 +42,6 @@ jq -c '.data.levels[]' "$response" | while read -r level; do
         exam_name=$(replace_spaces "$exam_name")
         
         # Créer le sous-dossier pour chaque examen
-        mkdir -p "$level_path/$exam_name"
-
-        # Lire les examens dans le niveau
-        echo "$exam" | jq -c '.rooms[]' | while read -r room; do
-            room_name=$(echo "$room" | jq -r '.name')
-            room_name=$(replace_spaces "$name")
-            
-            # Créer le sous-dossier pour chaque examen
-            mkdir -p "$exam_path/$room_name"
-        done
+        mkdir -p "$base_name/$level_path/$exam_name"
     done
 done
-
-echo "Dossiers créés avec succès."
-
